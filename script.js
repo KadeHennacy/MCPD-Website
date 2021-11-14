51555ce945a0d7537b3690e667dc068226aa883b
Kade Hennacy <kwh002@marietta.edu>
2021-11-14 17:29:18 -0500

    Fixed some visual issues on mobile

---
+++ script.js
@@ -0,0 +1,33 @@
+function sendTextMessage(){
+    var buildingNumber = document.getElementById('buildingNumberField').value;
+    var link = 'sms://+17402360217';
+    if(getOS() == 'Android') link += '?';
+    else{
+        link += '&'
+    }
+    link += 'body=Please send an escort to building number ';
+    link += buildingNumber;
+    window.location.href = link;
+    
+}
+function getOS() {
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
+
+    if (macosPlatforms.indexOf(platform) !== -1) {
+        os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+        os = 'Android';
+    } else if (!os && /Linux/.test(platform)) {
+        os = 'Linux';
+    }
+    return os;
+}
